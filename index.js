32dcb5a21edeab7f23179b448039b880b240a6a0
Piyush Kamlakar <you@example.com>
2026-02-15 21:14:46 +0530

    signup controller added

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const Dbconnect = require("./config/database");
+const User = require("./models/user");
+
+app.use(express.json());
+
+//signup controller//
+
+app.post("/signup",async(req,res)=>{
+    try {
+        const user = new user(req.body);
+        await user.save();
+        res.send("User Added Successfully");
+    } catch (error) {
+        res.status(400).send("Error saving the User:"+ error.message);
+    }
+})
 
 //calling the db connection//
 Dbconnect();
