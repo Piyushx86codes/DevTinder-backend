f43097f74334bfb4596eefed62d2626f4dbe1217
Piyush Kamlakar <you@example.com>
2026-02-25 14:40:36 +0530

    added the connection request api

--- routes/request.js
+++ routes/request.js
@@ -60,4 +60,38 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
   }
 })
 
+
+requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+  try {
+    const loggedInUser = req.user;
+    const {status,requestId} = req.params;
+    const allowedStatus = ["accepted","rejected"];
+    if(!allowedStatus.includes(status)){
+        return res.status(400).json({
+          message:"Status Not Allowed",
+        })
+    }
+    const connectionrequest = await Connectionrequest.findOne({
+      _id:requestId,
+      toUserId: loggedInUser._id,
+      status:"intrested",
+    })
+    if(!connectionrequest){
+      return res.status(404).json({
+        message:"Connection request Not Found",
+      })
+    }
+
+    connectionrequest.status = status;
+    const data = await connectionrequest.save();
+    res.json({message:"Connection Request"+ status, data});
+  } catch (error) {
+    res.status(400).json({
+      error:error.messsage,
+      success:false,
+      message:"failed",
+    })
+  }
+})
+
 module.exports = requestRouter;
